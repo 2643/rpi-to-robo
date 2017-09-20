7598ee5490a41ec9d3297930ebc330e11977c558
Adley Wong <adleywong59@gmail.com>
2017-09-20 15:57:33 -0700

    Update
    It is working now

--- TalkToPI.java
+++ TalkToPI.java
@@ -5,9 +5,15 @@ import edu.wpi.first.wpilibj.networktables.NetworkTable;
 public class TalkToPI 
 {
 	public static NetworkTable table = NetworkTable.getTable("Test");
-	
+	public static NetworkTable table1 = NetworkTable.getTable("Vision");
+
 	public static double testMet()
 	{
-		return table.getNumber("Hello", 0.0);
+		return table1.getNumber("Hello", 0.0);
+	}
+	
+	public static void placeVal(double val)
+	{
+		table.putNumber("Returned Val", val);
 	}
 }
